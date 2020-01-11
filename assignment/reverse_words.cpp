e78b70d3a6c4925a80a2d9b266e34129a563ea2c
raphaelradna <raphael.radna@gmail.com>
2020-01-11 01:07:40 -0800

    assignment 0: saved 3 lines

--- assignment/reverse_words.cpp
+++ assignment/reverse_words.cpp
@@ -11,23 +11,20 @@ int main() {
   while (true) {
     printf("Enter a sentence to reverse word-wise: ");
     string input;
-    getline(cin, input); //copies standard input into string "line"
+     getline(cin, input);
     if (!cin.good()) {
       printf("Done\n");
       return 0;
     }
-    string output;
-    string word;
+    string output, word;
     for (int i = 0; i < input.length(); i++) {
       if (input.at(i) != ' ') {
         word.insert(0, input, i, 1);
       } else {
-        word.append(" ");
-        output.append(word);
+        output.append(word + " ");
         word.clear();
       }
     }
-    output.append(word);
-    cout << output << endl;
+    cout << output << word << endl;
   }
 }
