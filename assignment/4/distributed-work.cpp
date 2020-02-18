566c7fe6d1c0072a9ff990fb02a2a78536b8172e
raphaelradna <raphael.radna@gmail.com>
2020-02-17 17:40:26 -0800

    assignment 4: integrate newer starter code

--- assignment/4/distributed-work.cpp
+++ assignment/4/distributed-work.cpp
@@ -55,11 +55,19 @@ struct Particle {
   Vec3f position, velocity, acceleration;
   float mass, massMod;
 };
-const int N = 1000;
+
+struct DrawableParticle {
+  Vec3f position;
+
+  void from(const Particle &that) { position.set(that.position); }
+};
+
+#define N (1000)
 
 struct SharedState {
-  uint16_t frameCount{0};
-  Particle particle[N]; // copy from a vector<Particle>
+  Pose cameraPose;
+  float background, pointSize;
+  DrawableParticle particle[N]; // copy from some vector<Particle>
 };
 
 Vec3f rv(float scale) {
@@ -75,7 +83,7 @@ void initParticle(Particle &p, Vec3f p_, Vec3f v_, Vec3f a_, float m_,
 
 // Inherit from DistributedApp and template it on the shared state data struct
 //
-class MyApp : public DistributedAppWithState<SharedState> {
+class AlloApp : public DistributedAppWithState<SharedState> {
   Parameter pointSize{"/pointSize", "", 0.5, "", 0.0, 5.0};
   Parameter timeStep{"/timeStep", "", 0.05, "", 0.01, 0.6};
   Parameter gravConst{"/gravConst", "", 0.75, "", 0, 1};
@@ -84,14 +92,13 @@ class MyApp : public DistributedAppWithState<SharedState> {
   Parameter scaleVal{"/scaleVal", "", 0.8, "", 0, 2};
   Parameter frequency{"/frequency", "", 1, "", 0, 5};
   Parameter depth{"/depth", "", 1, "", 0, 5};
+  ControlGUI gui;
 
   /* DistributedApp provides a parameter server. In fact it will
    * crash if you have a parameter server with the same port,
    * as it will raise an exception when it is unable to acquire
    * the port
    */
-
-  ControlGUI gui;
 
   ShaderProgram pointShader;
   Mesh mesh; // simulation state position is located in the mesh (positions are
@@ -114,7 +121,7 @@ class MyApp : public DistributedAppWithState<SharedState> {
 
     mesh.primitive(Mesh::POINTS);
 
-    for (int r = 0; r < N; r++) { // create 1000 points, put it into mesh
+    for (int _ = 0; _ < N; _++) { // create 1000 points, put it into mesh
       Particle p;
       float m = 5 + rnd::uniformS() * 10;
       initParticle(p, rv(5), rv(0.1), Vec3f(0), m, m);
@@ -163,7 +170,7 @@ class MyApp : public DistributedAppWithState<SharedState> {
                         slurp("../point-geometry.glsl"));
 
     reset();
-    nav().pos(0, 0, 10); // push camera back
+    nav().pos(0, 0, 50); // push camera back
   }
 
   bool freeze =
@@ -171,92 +178,103 @@ class MyApp : public DistributedAppWithState<SharedState> {
   int t = 0;
   void onAnimate(double dt) override {
     if (cuttleboneDomain->isSender()) {
-      state().frameCount++;
-      navControl().active(!isImguiUsingInput());
-    } else {
-    }
+      // state().frameCount++;
+      // navControl().active(!isImguiUsingInput());
 
-    if (freeze)
-      return;
+      if (freeze)
+        return;
 
-    // ignore the real dt (real number of seconds that have passed since the
-    // last call) and set the time step numerical simulation stability is due to
-    // small, regular timesteps
-    dt = timeStep; // simulation time, not wall time
+      dt = timeStep;
 
-    for (Particle p : particle) {
-      p.massMod = p.mass * (float)depth +
-                  sinf(((float)t * (float)frequency / 60.0f) * 6.283185308) *
-                      p.mass * (float)depth +
-                  0.01;
-    }
-
-    if (t >= 1 / (float)frequency * 60)
-      t = 0;
-    t++;
-
-    // *********** Calculate forces ***********
-
-    // gravity
-    for (int i = 0; i < N; i++) {
-      for (int j = 1 + i; j < N; j++) {
-
-        rnd::Random<> rng;
-        auto rv = [&](float scale) -> Vec3f {
-          return Vec3f(rng.uniformS(), rng.uniformS(), rng.uniformS()) * scale;
-        };
-
-        Particle &p1(particle.at(i));
-        Particle &p2(particle.at(j));
-        // Vec3f distance(mesh.vertices()[j] - mesh.vertices()[i]);
-        Vec3f distance(p2.position - p1.position);
-        Vec3f gravityVal = gravConst * p1.massMod * p2.massMod *
-                           distance.normalize() /
-                           pow(distance.mag(), 2); // F = G * m1 * m2 / r^2
-
-        // using a random multiplier here in order to show a more naturalistic,
-        // "swimming" motion of the particles
-
-        // acceleration[i] += gravityVal * rv(scaleVal) / massMod[i];
-        // acceleration[j] -= gravityVal * rv(scaleVal) / massMod[j];
-        p1.acceleration += gravityVal * rv(scaleVal) / p1.massMod;
-        p2.acceleration -= gravityVal * rv(scaleVal) / p2.massMod;
-
-        // drag -> stabilizes simulation
-
-        // force of drag is proportional to the opposite of velocity * small
-        // amount normally, it is v^2 -> can change the statement to
-        // vector[i].mag() * velocity[i] * 0.07; take a bit of acceleration away
-        // proportional to what the velocity is
-        p1.acceleration -= p1.velocity * dragFactor;
-
-        // limit acceleration
-        //
-        if (p1.acceleration.mag() > maxAccel)
-          p1.acceleration.normalize(maxAccel);
+      for (Particle p : particle) {
+        p.massMod = p.mass * (float)depth +
+                    sinf(((float)t * (float)frequency / 60.0f) * 6.283185308) *
+                        p.mass * (float)depth +
+                    0.01;
       }
+
+      if (t >= 1 / (float)frequency * 60)
+        t = 0;
+      t++;
+
+      // *********** Calculate forces ***********
+
+      // gravity
+      for (int i = 0; i < N; i++) {
+        for (int j = 1 + i; j < N; j++) {
+
+          rnd::Random<> rng;
+          auto rv = [&](float scale) -> Vec3f {
+            return Vec3f(rng.uniformS(), rng.uniformS(), rng.uniformS()) *
+                   scale;
+          };
+
+          Particle &p1(particle.at(i));
+          Particle &p2(particle.at(j));
+          // Vec3f distance(mesh.vertices()[j] - mesh.vertices()[i]);
+          Vec3f distance(p2.position - p1.position);
+          Vec3f gravityVal = gravConst * p1.massMod * p2.massMod *
+                             distance.normalize() /
+                             pow(distance.mag(), 2); // F = G * m1 * m2 / r^2
+
+          // using a random multiplier here in order to show a more
+          // naturalistic, "swimming" motion of the particles
+
+          // acceleration[i] += gravityVal * rv(scaleVal) / massMod[i];
+          // acceleration[j] -= gravityVal * rv(scaleVal) / massMod[j];
+          p1.acceleration += gravityVal * rv(scaleVal) / p1.massMod;
+          p2.acceleration -= gravityVal * rv(scaleVal) / p2.massMod;
+
+          // drag -> stabilizes simulation
+
+          // force of drag is proportional to the opposite of velocity * small
+          // amount normally, it is v^2 -> can change the statement to
+          // vector[i].mag() * velocity[i] * 0.07; take a bit of acceleration
+          // away proportional to what the velocity is
+          p1.acceleration -= p1.velocity * dragFactor;
+
+          // limit acceleration
+          //
+          if (p1.acceleration.mag() > maxAccel)
+            p1.acceleration.normalize(maxAccel);
+        }
+      }
+
+      // // drag -> stabilizes simulation
+      // for (int i = 0; i < N; i++) {
+      //   // force of drag is proportional to the opposite of velocity * small
+      //   // amount normally, it is v^2 -> can change the statement to
+      //   // vector[i].mag() * velocity[i] * 0.07; take a bit of acceleration
+      //   away
+      //   // proportional to what the velocity is
+      // acceleration[i] -= velocity[i] * dragFactor;
+      // }
+
+      // // limit acceleration
+      // for (int i = 0; i < acceleration.size(); i++) {
+      //   float m = acceleration[i].mag();
+      //   if (m > maxAccel) {
+      //     acceleration[i].normalize(maxAccel);
+      //     // cout << "Limiting Acceleration: " << m << " -> " <<
+      //     (float)maxAccel
+      //     //      << endl;
+      //   }
+      // }
+
+      //
+      // Copy all the agents into shared state;
+      for (unsigned i = 0; i < N; i++) {
+        state().particle[i].from(particle[i]);
+      }
+      state().cameraPose.set(nav());
+      state().background = 0;
+      state().pointSize = pointSize.get();
+
+    } else {
+      //
+      // use the camera position from the simulator
+      nav().set(state().cameraPose);
     }
-
-    // // drag -> stabilizes simulation
-    // for (int i = 0; i < N; i++) {
-    //   // force of drag is proportional to the opposite of velocity * small
-    //   // amount normally, it is v^2 -> can change the statement to
-    //   // vector[i].mag() * velocity[i] * 0.07; take a bit of acceleration
-    //   away
-    //   // proportional to what the velocity is
-    // acceleration[i] -= velocity[i] * dragFactor;
-    // }
-
-    // // limit acceleration
-    // for (int i = 0; i < acceleration.size(); i++) {
-    //   float m = acceleration[i].mag();
-    //   if (m > maxAccel) {
-    //     acceleration[i].normalize(maxAccel);
-    //     // cout << "Limiting Acceleration: " << m << " -> " <<
-    //     (float)maxAccel
-    //     //      << endl;
-    //   }
-    // }
 
     // Integration -> don't mess with this
 
@@ -322,7 +340,8 @@ class MyApp : public DistributedAppWithState<SharedState> {
 
   void onDraw(Graphics &g) override {
     float scaledSize = pointSize / 50;
-    g.clear();
+    float f = state().background;
+    g.clear(f, f, f);
     g.shader(pointShader);
     g.shader().uniform("pointSize", scaledSize);
     g.shader().uniform(
@@ -336,16 +355,15 @@ class MyApp : public DistributedAppWithState<SharedState> {
     g.draw(mesh);
 
     // Draw th GUI on the simulator only
-    if (isPrimary()) {
+    if (cuttleboneDomain->isSender()) {
       gui.draw(g);
+      // if (isPrimary()) {
+      //   gui.draw(g);
     }
   }
 };
 
-int main() {
-  MyApp app;
-  app.start();
-}
+int main() { AlloApp().start(); }
 
 // slurp definition
 string slurp(string fileName) {
